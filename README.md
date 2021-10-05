3f6ec0ab808852d8b7a86fa2f90d914484313075
Matt Lane <84758670+LaneTrain47@users.noreply.github.com>
2021-10-05 12:39:29 -0400

    20211005_123900
    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ShadowReWind
+ ShadowReWind
---
+++ js/site.js
@@ -0,0 +1,13 @@
+// The function below displays a message to the user
+
+function displayMessage() {
+
+    //declare the variable
+    let msg = "";
+
+    //retrieve the value from the input field
+    msg = document.getElementById("message").value;
+
+    //display the message within the variable to the user
+    alert(msg);
+}
