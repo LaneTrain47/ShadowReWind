1cbe86729266339aea66c73e450085c2fd30a087
Matt Lane <84758670+LaneTrain47@users.noreply.github.com>
2021-10-08 22:38:07 -0400

    20211008_223700

--- js/site.js
+++ js/site.js
@@ -1,13 +1,43 @@
-// The function below displays a message to the user
+// Get starting values from the input field on the page
+// Controller function
+function getString() {
+    //Get the user's input
+    let userString = document.getElementById("userString").value;
+    //Version 2 - Test the user's input for length
 
-function displayMessage() {
+    //Reverse the user's input - this is a separate function
+    let revString = reverseString(userString);
 
-    //declare the variable
-    let msg = "";
+    //Display the result - this is a separate function
+    displayString(revString);
+}
 
-    //retrieve the value from the input field
-    msg = document.getElementById("message").value;
+function reverseString(userString) {
+    let startValue = userString.length - 1;
+    let endValue = 0;
 
-    //display the message within the variable to the user
-    alert(msg);
+    //User input = Frank
+    //Frank is a string AND an array of characters
+    //[F, r, a, n, k]
+
+    let revString = "";
+
+    //Since this is a decremented for loop, the startValue must be higher than the endValue
+    for (let i = startValue; i >= endValue; i--) {
+        revString += userString[i];
+    };
+
+    //Loop 1 - "k"
+    //Loop 2 - "kn"
+    //Loop 3 - "kna"
+    //Loop 4 - "knar"
+    //Loop 5 - "knarF"
+    return revString;
+
+}
+
+function displayString(revString) {
+    let output = document.getElementById("results");
+
+    output.innerHTML = revString;
 }
